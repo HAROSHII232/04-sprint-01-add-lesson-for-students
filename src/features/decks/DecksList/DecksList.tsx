a86f3f108f57b5a0540d4b1d33d113ac7e961655
Ihar Voitau <igorgrime@gmail.com>
2024-08-21 22:20:54 +0300

    feat: solve task 1

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,5 +1,15 @@
+import { useEffect, useState } from 'react'
+import { decksAPI, DecksType } from '../decks-api'
 import s from './DecksList.module.css'
 
 export const DecksList = () => {
+  const [state, setState] = useState<DecksType[]>()
+  useEffect(() => {
+    decksAPI.getDecks().then((res) => {
+      setState(res.data.items)
+    })
+  }, [])
+  console.log(state)
+
   return <ul className={s.list}></ul>
 }
--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -6,3 +6,33 @@ export const instance = axios.create({
     'x-auth-skip': true,
   },
 })
+
+export const decksAPI = {
+  getDecks() {
+    return instance.get<GetDecksResponseType>('/v2/decks')
+  },
+}
+export type GetDecksResponseType = {
+  items: DecksType[]
+  pagination: {
+    currentPage: number
+    itemsPerPage: number
+    totalPages: number
+    totalItems: number
+  }
+}
+export type DecksType = {
+  isFavorite: boolean
+  author: {
+    id: string
+    name: string
+  }
+  id: string
+  userId: string
+  name: string
+  isPrivate: boolean
+  cover: string
+  created: Date
+  updated: Date
+  cardsCount: number
+}
